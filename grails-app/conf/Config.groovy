978a19163e6cdb7865bec3108b29ec4cbe330f2e
Cedric G Hurst II <cedric@spantree.net>
2012-01-18 20:19:15 -0600

    updating logging information and setting grails.scan.disable property in Config.groovy (for testing purposes)

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -1,5 +1,7 @@
 // configuration for plugin testing - will not be included in the plugin zip
  
+grails.scan.disable = true
+
 log4j = {
     // Example of changing the log pattern for the default console
     // appender:
@@ -8,13 +10,14 @@ log4j = {
     //    console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
     //}
 
-    error  'org.codehaus.groovy.grails.web.servlet',  //  controllers
+	debug 'org.codehaus.groovy.grails.plugins'
+
+    info  'org.codehaus.groovy.grails.web.servlet',  //  controllers
            'org.codehaus.groovy.grails.web.pages', //  GSP
            'org.codehaus.groovy.grails.web.sitemesh', //  layouts
            'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
            'org.codehaus.groovy.grails.web.mapping', // URL mapping
            'org.codehaus.groovy.grails.commons', // core / classloading
-           'org.codehaus.groovy.grails.plugins', // plugins
            'org.codehaus.groovy.grails.orm.hibernate', // hibernate integration
            'org.springframework',
            'org.hibernate',
