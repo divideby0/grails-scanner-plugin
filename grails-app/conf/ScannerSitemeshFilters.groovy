d93c0208d774585aa4858701e3eea1766d92e51c
Cedric G Hurst II <cedric@spantree.net>
2012-01-19 19:42:29 -0600

    adding behavior to disable the sitemesh config refresh behavior as well

---
+++ grails-app/conf/ScannerSitemeshFilters.groovy
@@ -0,0 +1,40 @@
+import org.codehaus.groovy.grails.commons.ConfigurationHolder
+
+class ScannerSitemeshFilters {
+	Set<Integer> disabledRefresh = []
+	
+	def filters = {
+		/*
+		 * The grails sitemesh implementation refreshes its configuration outside the normal scope of
+		 * the resource reloading.  If general scanning is disabled (or sitemesh scanning is disabled
+		 * specifically) this filter will disable the refresh behavior.
+		 */
+		
+		disableRefreshFilter(controller: '*', action: '*') {
+			before = {
+				Boolean disableRefresh = (
+					ConfigurationHolder.config.grails?.scan?.disable
+					|| ConfigurationHolder.config.grails?.scan?.sitemesh?.disable
+				)
+				
+				if(disableRefresh) {
+					def sitemeshFactory = servletContext.getAttribute('sitemesh.factory')
+					
+					if(sitemeshFactory && !disabledRefresh.contains(sitemeshFactory.hashCode())) {
+						log.info "Disabling refresh behavior for sitemesh factory ${sitemeshFactory}"
+						
+						sitemeshFactory.metaClass.refresh = { ->
+							long time = System.currentTimeMillis()
+							configLastCheck = time;
+							
+							if(configFile != null && !configLastCheck) {
+								loadConfig()
+							}
+						}
+						disabledRefresh << sitemeshFactory.hashCode()
+					}
+				}
+			}
+		}
+	}
+}
